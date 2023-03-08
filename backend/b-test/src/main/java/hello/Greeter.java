ed618f4f48e54d1c3d98e3eb0c04202a0e1b3916
Hari Haran CS <hariharan.cs@Haris-MacBook-Pro.local>
2023-03-08 12:26:30 +0530

    Code Changes

--- backend/b-test/src/main/java/hello/Greeter.java
+++ backend/b-test/src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-    public String sayHello() {
-        return "This is B-test Project";
-    }
+  public String sayHello() {
+    return "Hello world!";
+  }
 }
--- backend/c-test/src/main/java/hello/Greeter.java
+++ backend/c-test/src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-    public String sayHello() {
-        return "This is C-Test project";
-    }
+  public String sayHello() {
+    return "Hello world!";
+  }
 }
